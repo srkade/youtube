c9854482bc4d602abc1d26e969b0561cabf98f8c
Shrikrushna Ade <shrikrushnaade766@gmail.com>
2024-01-20 16:29:29 +0530

    fix all register controller bigs

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,5 @@
 import mongoose,{Schema} from "mongoose";
-import { jwt } from "jsonwebtoken";
+import Jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
@@ -58,7 +58,7 @@ userSchema.methods.isPasswordCorrect -async function(password){
   return await  bcrypt.compare(password, this.password)
 }
 userSchema.methods.generateAccessToken=function (){
-    return jwt.sign(
+    return Jwt.sign(
         {
             _id: this._id,
             email:this.email,
@@ -73,7 +73,7 @@ userSchema.methods.generateAccessToken=function (){
     )
 }
 userSchema.methods.generateRefreshToken=function (){
-    return jwt.sign(
+    return Jwt.sign(
         {
             _id: this._id,
         },
