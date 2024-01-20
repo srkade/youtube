9242fb956ffa2bc66bfbd1c3f28a5afc2800c89d
Shrikrushna Ade <shrikrushnaade766@gmail.com>
2024-01-20 12:15:51 +0530

    all setup configured

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import asyncHandler from "../utils/asyncHandler.js"
+const  registerUser=asyncHandler(async(req,res) =>{
+   return res.status(200).json({
+        message: "this is coder "
+    })
+})
+
+export {registerUser}
--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB= async ()=>{
     try {
       const connectionInstance=  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-      console.log(`\n MONGOdb connected : ${connectionInstance}`)
+      console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("Mongodb connection error ", error)
         process.exit(1);
